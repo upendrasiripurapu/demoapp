4290c4701d0f61327fa11754b2fb1ff5f54ce745
upendra prasad <upendra.siripurapu@gmail.com>
2015-10-11 12:43:49 +0530

    Updated demo code

--- DemoProject/ViewController.swift
+++ DemoProject/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var changeTextButton: UIButton!
+    
+    @IBAction func changeDataOnLabel(sender: AnyObject) {
+        mainLabel.text = "Hello you have change the data successfully";
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func openNewWindow(sender: AnyObject) {
+    }
+    
 
 }
 
